992d36da381e7be5b91667f1454d3be6cb5a440b
ar-ko <arteamko@icloud.com>
2020-05-21 15:53:18 +0300

    Added PreferredNozzleType checkboxes logic

--- lib/models/preferred_nozzle_type.dart
+++ lib/models/preferred_nozzle_type.dart
@@ -16,6 +16,48 @@ class PreferredNozzleType {
     bool rotators,
     bool any,
   }) {
+    if (any != null) {
+      statics = false;
+      rotors = false;
+      rotators = false;
+    }
+
+    if (statics != null && statics) {
+      if (this.rotators && this.rotors) {
+        any = true;
+        statics = false;
+        rotors = false;
+        rotators = false;
+      }
+      if (this.any) {
+        any = false;
+      }
+    }
+
+    if (rotators != null && rotators) {
+      if (this.statics && this.rotors) {
+        any = true;
+        statics = false;
+        rotors = false;
+        rotators = false;
+      }
+      if (this.any) {
+        any = false;
+      }
+    }
+
+    if (rotors != null && rotors) {
+      if (this.statics && this.rotators) {
+        any = true;
+        statics = false;
+        rotors = false;
+        rotators = false;
+      }
+      if (this.any) {
+        any = false;
+      }
+    }
+
     return PreferredNozzleType(
       statics: statics ?? this.statics,
       rotors: rotors ?? this.rotors,
